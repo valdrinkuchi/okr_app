351d2cec6838044ee839905052aa712d4da5c02a
Valdrin Kuchi <valdrinkuchi@gmail.com>
2022-03-09 10:41:36 +0100

    Update rails

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -4,10 +4,12 @@ class GoalTest < ActiveSupport::TestCase
   setup do
     @user = FactoryBot.create(:user)
     @status = FactoryBot.create(:key_result_status, status: 'completed')
+    @status2 = FactoryBot.create(:key_result_status, status: 'in progress')
     @goal = FactoryBot.create(:goal, user: @user)
     @key_result = FactoryBot.create(:key_result, title: 'Test Title', goal: @goal, key_result_status: @status)
+    @key_result = FactoryBot.create(:key_result, title: 'Test Title2', goal: @goal, key_result_status: @status2)
   end
-  test 'Should return progress progress' do
-    assert_equal '100.00%', @goal.progress
+  test 'Should return goal progress' do
+    assert_equal '50.00%', @goal.progress
   end
 end
